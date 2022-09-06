d7b216c57bedd5b316832108c54cc1467a22831a
DaHyun <dahyun123410@gmail.com>
2022-09-06 16:24:06 +0900

    0906 작업

--- 03. tab/js/tab.js
+++ 03. tab/js/tab.js
@@ -1,3 +1,4 @@
+//nav-tit
 $(function(){
     $('.nav-tit').on('mouseover',function(){
             //마우스에 올린 nav-tit의 li 안의 sub-wrap은 .on 추가
@@ -19,5 +20,30 @@ $(function(){
         $(this).removeClass("on");
     });
 });
+
+//방법01. sub-tit 클릭 시 sub-nav-cont 나오게
+// $(function(){
+//     $('.sub-nav-cont').hide(); //sub-nav-cont 숨긴다
+//     $('.sub-tit a').click(function(){ // 각 sub-tit 클릭 시 
+//         $('.sub-nav-cont').hide().filter(this.hash).fadeIn(200); // sub-tit 과 동일한 id의.sub-nav-cont 나타나기
+//         $('.sub-tit a').removeClass('active'); // 각 sub-tit의 active 클래스 삭제
+//         $(this).addClass('active'); // 클릭한 sub-tit의 active 클래스 추가
+//     })
+// });
+
+//방법02. sub-tit 클릭 시 sub-nav-cont 나오게
+$(function(){
+    $('.sub-tit a').click(function(){ 
+        var activeTab = $(this).attr("href");
+
+        $('.sub-tit a').removeClass('active'); 
+        $('.sub-nav-cont').removeClass('on');
+
+        $(this).addClass('active'); 
+        $(activeTab).addClass('on');
+    })
+});
+
+
 
 
